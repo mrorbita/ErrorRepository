e8f65d4ef9c35a7e81f654eeffe1acd2c18eadb9
mrAlex <mrorbita@gmail.com>
2015-03-07 15:06:59 +0300

    Update jobDone.js

--- jobDone.js
+++ jobDone.js
@@ -1,7 +1,7 @@
-This text is to create a complie error
-
+//This text is to create a complie error
+// such lines are not creating erros in javascript
 function jobDone() {
-
+        (
         return false;
 }
 exports.main = jobDone;
